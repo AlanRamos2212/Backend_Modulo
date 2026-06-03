42822879fc7ef6972cc691afba3ceef943c93dcd
AlanRamos2212 <alanperex112@gmail.com>
2026-06-03 17:37:56 -0600

    Commit Inicial Spring

---
+++ divisiones/src/main/java/com/module/divisiones/DivisionesApplication.java
@@ -0,0 +1,13 @@
+package com.module.divisiones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DivisionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DivisionesApplication.class, args);
+	}
+
+}
---
+++ divisiones/src/test/java/com/module/divisiones/DivisionesApplicationTests.java
@@ -0,0 +1,13 @@
+package com.module.divisiones;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DivisionesApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
